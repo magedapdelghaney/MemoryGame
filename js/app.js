55a19c702b3f299352796a772f48cbb23c9ca172
Maged AbdelGhany <Maged.AbdelGhany@itworx.com>
2018-09-06 18:54:01 +0200

    adding moves logic for each click

--- js/app.js
+++ js/app.js
@@ -2,46 +2,88 @@
  * Create a list that holds all of your cards
  */
 
-var cards = ["diamond","paper-plane-o","bolt","leaf","bicycle","cube","anchor"]
-
-
+var cards = ["diamond","paper-plane-o","bomb","bolt","leaf","bicycle","cube","anchor","diamond","paper-plane-o","bolt","leaf","bicycle","cube","anchor","bomb"];
 
 var newCards;
 
 var openCards = [];
 
-// function flip(e) {
-//     e.target.classList.toggle("show");
-//     e.target.classList.toggle("open");
-//     openCards.push(e);
-//     checkEquality();
-// }
 
-// function checkEquality() {
-//     if (openCards.length > 1) {
-//         console.log(openCards[0].classList);
-//         if (openCards[0].classList === openCards[1].classList) {
-//             alert("hey");
-//         }
+addingCards(cards);
+function addingCards(cardsList)
+{
+    const fragment = document.createDocumentFragment();  // ← uses a DocumentFragment instead of a <div>
+    for (let index = 0; index < cardsList.length; index++) {
 
-//     }
-// // }
+        var card= document.createElement("li");
+        card.classList.add("card");
+
+        cardIcon=document.createElement("li");
+        cardIcon.classList.add("fa","fa-"+cardsList[index])
+        card.appendChild(cardIcon);
+        card.addEventListener("click", flip)
+
+        fragment.appendChild(card);
+
+    }
+    document.querySelector(".deck").appendChild(fragment);
+}
+ function flip(e) {
+
+     e.target.classList.toggle("show");
+     e.target.classList.toggle("open");
+
+     openCards.push(e);
+
+     if (openCards.length > 1)
+     {
+     checkEquality();
+     }
+ }
+
+ function flipBack(e) {
+
+    e.target.classList.toggle("show");
+    e.target.classList.toggle("open");
+    e.target.classList.toggle("not-match");
+    openCards=[];
+
+}
+
+ function checkEquality() {
+
+    var firstCard=openCards[0];
+    var secondCard=openCards[1];
+
+         if (firstCard.target.childNodes[0].classList[1] === secondCard.target.childNodes[0].classList[1])
+         {
+            firstCard.target.classList.add('match');
+             secondCard.target.classList.add('match');
+             openCards=[];
+         }
+
+         else
+         {
+            firstCard.target.classList.add('not-match');
+            secondCard.target.classList.add('not-match');
+            flipBack(firstCard);
+            flipBack(secondCard);
+
+
+         }
+
+
+
+
+ }
 
 // for (var i = 0; i < cards.length; i++) {
 //     cards[i].addEventListener("click", flip)
 // }
-newCards = shuffle(cards);
 console.log(cards);
+newCards = shuffle(cards);
+
 console.log(newCards);
-
-
-for (var r = 0; r < cards.length; r++) {
-    console.log(cards[r].innerHTML)
-}
-console.log("-----------------")
-for (var t = 0; t < newCards.length; t++) {
-    console.log(newCards[t].innerHTML)
-}
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
